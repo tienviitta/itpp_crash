70e8538edcd9e1d0a21989b63137cf22a806b87a
Väisänen, Petri <Petri.Vaisanen@nordicsemi.no>
2022-12-15 15:40:02 +0200

    Communication example working

--- main.cpp
+++ main.cpp
@@ -1,30 +1,39 @@
-#include <itpp/itbase.h>
+#include <itpp/itcomm.h>
 using namespace itpp;
 // These lines are needed for use of cout and endl
 using std::cout;
 using std::endl;
 int main()
 {
-    // Declare vectors and matricies:
-    vec a, b, c;
-    mat A, B;
-    // Use the function linspace to define a vector:
-    a = linspace(1.0, 2.0, 10);
-    // Use a string of values to define a vector:
-    b = "0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0";
-    // Add two vectors:
-    c = a + b;
-    // Print results:
-    cout << "a = " << a << endl;
-    cout << "b = " << b << endl;
-    cout << "c = " << c << endl;
-    // Use a string to define a matrix:
-    A = "1.0 2.0;3.0 4.0";
-    // Calculate the inverse of matrix A:
-    B = inv(A);
-    // Print results:
-    cout << "A = " << A << endl;
-    cout << "B = " << B << endl;
+    // Scalars
+    int N;
+    double N0;
+    // Vectors
+    bvec bits, dec_bits;
+    vec symbols, rec;
+    // Classes
+    BPSK bpsk; // The BPSK modulator/debodulator class
+    BERC berc; // The Bit Error Rate Counter class
+    // Init
+    N = 500000; // The number of bits to simulate
+    N0 = 1;     // 0 dB SNR
+    // Randomize the random number generator
+    RNG_randomize();
+    // Generate the bits:
+    bits = randb(N);
+    // Do the BPSK modulation
+    bpsk.modulate_bits(bits, symbols);
+    // Add the AWGN
+    rec = symbols + sqrt(N0 / 2) * randn(N);
+    // Decode the received bits
+    bpsk.demodulate_bits(rec, dec_bits);
+    // Count the number of errors
+    berc.count(bits, dec_bits);
+    // Print the results
+    cout << "There were " << berc.get_errors() << " received bits in error." << endl;
+    cout << "There were " << berc.get_corrects() << " correctly received bits." << endl;
+    cout << "The error probability was " << berc.get_errorrate() << endl;
+    cout << "The theoretical error probability is " << 0.5 * erfc(1.0) << endl;
     // Exit program:
     return 0;
 }
